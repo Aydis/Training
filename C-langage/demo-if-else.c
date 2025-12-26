cfe67c3965e2c67cca8f8633ffebb74b76042793
Aydis <pasidya@gmail.com>
2025-12-26 10:45:29 +0000

    startqemu: fix C-langage files

---
+++ C-langage/demo-if-else.c
@@ -0,0 +1,13 @@
+
+
+void aydis(){
+
+    int i = 1;
+    int j = 2;
+
+    while (i==1)
+    {
+        j = j+1;
+    }
+    return;
+}
--- C-langage/demo-if-else.s
+++ C-langage/demo-if-else.s
@@ -1,4 +1,4 @@
-	.file	"double.c"
+	.file	"demo-if-else.c"
 	.option nopic
 	.attribute arch, "rv32i2p1"
 	.attribute unaligned_access, 0
@@ -11,20 +11,22 @@ aydis:
 	addi	sp,sp,-32
 	sw	s0,28(sp)
 	addi	s0,sp,32
-	lui	a5,%hi(.LC0)
-	lw	a4,%lo(.LC0)(a5)
-	lw	a5,%lo(.LC0+4)(a5)
-	sw	a4,-24(s0)
+	li	a5,1
+	sw	a5,-24(s0)
+	li	a5,2
 	sw	a5,-20(s0)
-	li	a5,0
-	mv	a0,a5
+	j	.L2
+.L3:
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-20(s0)
+.L2:
+	lw	a4,-24(s0)
+	li	a5,1
+	beq	a4,a5,.L3
+	nop
 	lw	s0,28(sp)
 	addi	sp,sp,32
 	jr	ra
 	.size	aydis, .-aydis
-	.section	.rodata
-	.align	3
-.LC0:
-	.word	-1786706395
-	.word	1079962435
 	.ident	"GCC: (13.2.0-11ubuntu1+12) 13.2.0"
