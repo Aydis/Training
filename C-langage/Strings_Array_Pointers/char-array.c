5caf79ff52505776173a607cadead342413b9b80
Aydis <pasidya@gmail.com>
2026-01-01 18:42:06 +0000

    Strings_Array_Pointers

--- C-langage/Strings_Array_Pointers/char-array.c
+++ C-langage/Strings_Array_Pointers/char-array.c
@@ -2,7 +2,7 @@
 
 int main () {
 
-    char *str = "aydis";
+    /*char *str = "aydis";
     char z[] = {'h','z'}; // null-terminated character array
     char x[] = {'a', 'y','d','i','s'}; // null-terminated character array
     char y[] = {'h','z'}; // not null-terminated character array
@@ -10,8 +10,40 @@ int main () {
     printf("str: %s\n", str); // print the string
     printf("x: %s\n", x); // print the string
     printf("y: %s\n", y); // print the string
-    printf("z: %s\n", z); // print the string
+    printf("z: %s\n", z); // print the string*/
 
-    //printf("size of x: %ld\n", sizeof(x)); // size of the entire array (2 bytes)
+    char abc = 'Z';
+    char *ptr;
+    ptr = &abc; // pointer to char variable
+
+    char number[4];
+
+    int *in_ptr;
+
+    in_ptr= &number[0]; // pointer to char array
+
+    //printf("adresse of in_ptr: %p\n", in_ptr);
+    //printf("*in_ptr : %d\n", *in_ptr);
+
+    *in_ptr = 0x48492100; // assign value to the first element of the array
+    printf("*in_ptr after assignment: %c\n", *in_ptr);
+
+    int i=0;
+
+    while (i<4) {
+        printf("number[%d]: %c\n", i, number[i]);
+        i++;
+    }
+    {
+        /* code */
+    }
+    
+
+    /*printf("size of abc: %ld\n", sizeof(abc)); // size of the entire array (2 bytes)
+    printf("size of ptr: %ld\n", sizeof(ptr)); // size of the entire array (8 bytes on 64-bit systems)  
+    printf("adresse of abc: %p\n", &abc);
+    printf("value of ptr: %p\n", ptr);
+    printf("value pointed by ptr: %c\n", *ptr);
+    printf("value of abc: %c\n", abc);  */
     return 0;
 }
